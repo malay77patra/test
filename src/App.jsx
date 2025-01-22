fea1cd72d98a87115326413cb0a5ecd09e028d1d
malay77patra <malay77patra@gmail.com>
2025-01-22 20:24:09 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
-import { useState } from 'react'
-import { Component } from 'react'
-import './App.css'
+import React from "react"
 
-// using function
+class Counter extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: this.props.starer
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={() => this.changeCount(1)}>+</button>
+        <button>{this.state.count}</button>
+        <button onClick={() => this.changeCount(-1)}>-</button>
+      </>
+    )
+  }
+
+  changeCount(val) {
+    this.setState({ count: this.state.count + val })
+    this.setState({ count: this.state.count + val })
+    this.setState({ count: this.state.count + val })
+    this.setState({ count: this.state.count + val })
+    this.setState({ count: this.state.count + val })
+    this.setState({ count: this.state.count + val })
+  }
+
+}
+
+
 function App() {
-  const [count, setCount] = useState(767973);
 
   return (
     <>
-      <h1>Number is {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Counter starer={10} />
     </>
   )
 }
 
 export default App
-
-// // using class
-// class App2 extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       count: 0,
-//       name: 'John'
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <h1>Number is {this.state.count}</h1>
-//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>Increment</button>
-//       </>
-//     )
-//   }
-// }
